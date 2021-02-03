1edf6f4ce809102d5cfdf3e92ba54719e3a10b5e
Krutik <krutikshah6@gmail.com>
2021-02-03 16:26:59 +0530

    Deploy2

--- routes/files.js
+++ routes/files.js
@@ -66,10 +66,10 @@ router.post('/send', async (req, res) => {
         text: `${emailFrom} shared a file with you.`,
         html: require('../services/emailTemplate')({
                   emailFrom, 
-                  downloadLink: `${process.env.APP_BASE_URL}files/${file.uuid}`,
+                  downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
                   size: parseInt(file.size/1000) + ' KB',
                   expires: '24 hours'
-              })
+              })    
             });
             return res.send({ success: true})
   
