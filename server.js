f3d0cbfbd93af4eab1063e9f21229ff65af36ae8
Krutik <krutikshah6@gmail.com>
2021-02-03 22:46:09 +0530

    cors

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.static('public'))
 
 
+
 connectDB() 
+
+
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    
+}
+
+app.use(cors(corsOptions));
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.json())
 app.set('view engine', 'ejs')
