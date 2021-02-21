25f189d4e121a6e247fe3f2a2d32fd4c592b73aa
Krutik <krutikshah6@gmail.com>
2021-02-22 01:13:35 +0530

    cors

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.static('public'))
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 
 connectDB() 
